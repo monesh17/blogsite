24d7808373f1b6eff346f075230f30f4dad9dcbc
Monesh Periya Maruthu <pmonesh@athenahealth.com>
2021-07-25 16:34:18 +0530

    Initial commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout';
+import Head from 'next/head'
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1>404 - Blog post not available </h1>
+        </Layout>
+      )
+}
