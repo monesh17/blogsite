e364aeb8066f94e8521f888260a2d8509605024b
Monesh Periya Maruthu <pmonesh@athenahealth.com>
2021-07-26 06:47:21 +0530

    Create Blog build fix for window

--- pages/blog/create.tsx
+++ pages/blog/create.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { EditorState, ContentState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import Head from 'next/head';
 import Layout from '../../components/layout';
 import styles from './create.module.css';
+
+import dynamic from "next/dynamic";
+const Editor: any = dynamic(
+  () => {
+      return import("react-draft-wysiwyg")
+         .then((mod) =>mod.Editor);
+   },{ loading: () => null, ssr: false }
+);
 
 export default function create() {
   const [editorState, setEditorState] = useState(() =>
