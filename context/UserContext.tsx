a6921b334d3a209efab825b0c700678674642e3c
Monesh Periya Maruthu <pmonesh@athenahealth.com>
2021-09-13 06:27:51 +0530

    changed json structure

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -44,7 +44,7 @@ export function AuthProvider({ children }: Props) {
       method: 'POST',
       headers: { 'Content-Type': 'application/json',
       'Access-Control-Allow-Credentials': 'true' },
-      body: JSON.stringify({ userName, password }),
+      body: JSON.stringify({ 'userName':userName, 'password':password }),
     });
     setUser({
       userName: userName,
