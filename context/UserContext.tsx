a8d4e744b06545d22b02f13cb3e5185549d971f4
Monesh Periya Maruthu <pmonesh@athenahealth.com>
2021-09-13 06:31:34 +0530

    reverted json change

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -44,7 +44,7 @@ export function AuthProvider({ children }: Props) {
       method: 'POST',
       headers: { 'Content-Type': 'application/json',
       'Access-Control-Allow-Credentials': 'true' },
-      body: JSON.stringify({ 'userName':userName, 'password':password }),
+      body: JSON.stringify({ userName, password }),
     });
     setUser({
       userName: userName,
